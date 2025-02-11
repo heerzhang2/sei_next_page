c90da6b5f0e07fcc3f1ee4fde4f6ad76ea166089
herzhang <herzhang@163.com>
2025-02-11 17:19:10 +0800

    试验next-auth4

---
+++ mainProj/utils/auth-options.ts
@@ -0,0 +1,99 @@
+import { Adapter } from 'next-auth/adapters';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import type { NextAuthOptions } from 'next-auth';
+// import { PrismaAdapter } from '@auth/prisma-adapter';
+// import { Routes } from '@/config/routes';
+// import bcrypt from 'bcrypt';
+import { isDevelopment } from './env';
+// import { prisma } from '@/lib/prisma';
+
+export const authOptions: NextAuthOptions = {
+  // adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+    CredentialsProvider({
+      name: 'E-mail',
+      credentials: {
+        email: { label: 'E-mail', type: 'email', placeholder: 'Enter e-mail' },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: 'Enter password'
+        }
+      },
+      async authorize(credentials, req) {
+        if (!credentials || !credentials.password) {
+          return null;
+        }
+        const user ={};
+        // const user = await prisma.user.findUnique({
+        //   where: {
+        //     email: credentials.email
+        //   }
+        // });
+
+        // if (!user) throw new Error('User not found');
+        //
+        // const passwordValid = await bcrypt.compare(
+        //   credentials.password,
+        //   user.password
+        // );
+        //
+        // if (!passwordValid) {
+        //   throw new Error('Invalid password');
+        // }
+
+        return user;
+      }
+    })
+  ],
+  callbacks: {
+    // JWT is triggered before session
+    // returns the user only on sign in action
+    async jwt({ token, user, session, trigger }) {
+      console.log("callbacks/jwt::USER->{}", user);
+      if (trigger === 'update' && session?.name) {
+        token.name = session.name;
+      }
+
+      if (user) {
+        return {
+          ...token,
+          id: user.id,
+          address: user.address,
+          role: user.role
+        };
+      }
+
+      const newUser = await prisma.user.update({
+        where: {
+          id: token.id
+        },
+        data: {
+          name: token.name
+        }
+      });
+
+      return token;
+    },
+    async session({ session, token }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+          address: token.address,
+          name: token.name,
+          role: token.role
+        }
+      };
+    }
+  },
+  pages: {
+    signIn: '/login'
+  },
+  session: {
+    strategy: 'jwt'
+  },
+  debug: isDevelopment,
+  secret: process.env.NEXTAUTH_SECRET
+};
