257ccebcb3a503cf270942369019976dd7e0245b
herzhang <herzhang@163.com>
2025-02-14 17:04:52 +0800

    react-hook-form以及@hookform/resolvers/zod 不考虑用

--- mainProj/component/SignInForm.tsx
+++ mainProj/component/SignInForm.tsx
@@ -1,86 +1,49 @@
 'use client';
 
+import {useActionState, useEffect, useRef, useState,} from 'react';
 import FieldSetWithStatus from '@/component/FieldSetWithStatus';
 import Container from '@/component/Container';
 import SubmitButtonWithStatus from '@/component/SubmitButtonWithStatus';
-import {
-  useActionState,
-  useEffect,
-  useRef,
-  useState,
-} from 'react';
-// import { getAuthAction, signInAction } from './actions';
-// import ErrorNote from '@/components/ErrorNote';
 import {KEY_CALLBACK_URL, KEY_CREDENTIALS_SIGN_IN_ERROR, } from '.';
-import {redirect, useSearchParams} from 'next/navigation';
-// import { useAppState } from '@/state/AppState';
-// import { clsx } from 'clsx/lite';
+import {useSearchParams} from 'next/navigation';
 import { FiLock } from 'react-icons/fi';
 import {getAuthAction, } from "@/action/auth";
-// import {getAuthAction, signInAction} from "@/action/auth";
-import {useAppState} from "@/action/AppState";
 import ErrorNote from "@/component/ErrorNote";
 import useLoginMutation from "@/component/useLoginMutation";
-import {PATH_ADMIN_PHOTOS} from "@/site/paths";
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import {z} from "zod";
 import { revalidatePath } from 'next/cache'
+import {useAppState} from "@/action/AppState";
+
+// import { z } from "zod";
+// import { zodResolver } from '@hookform/resolvers/zod';
+// import { useForm } from 'react-hook-form';             =感觉是罗嗦了
+// const FormSchema = z.object({
+//   email: z.string().email(),
+//   password: z.string().min(6),
+// });
+// export default function LoginForm() {
+//   const form = useForm<z.infer<typeof FormSchema>>({       =感觉可以学习的点？
+//     resolver: zodResolver(FormSchema),
+//     defaultValues: {email: '', password: '',},
+//   });
+// async function onSubmit(data: z.infer<typeof FormSchema>) {const res = await signIn('credentials', {}
 
 
 //密码hash 防止在服务后台泄密
 var sha256 = require('hash.js/lib/hash/sha/256');
 
 export default function SignInForm() {
-  const {useRelayEnvironment} = require('react-relay');
-  const environment = useRelayEnvironment();
   const router = useRouter();
   const params = useSearchParams();
-
   const { setUserEmail } = useAppState();
-
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [username, setUsername] = useState('');
   const {call:submitfunc, doing:isInFlight}= useLoginMutation();
 
-  async function 旧的onSubmit_Login(data: z.infer<typeof FormSchema>) {
-    console.log("onSubmit_Login:", data);
-    const res = await signIn('credentials', {
-      redirect: false,
-      email: data.email,
-      password: data.password,
-    });
-    if (res?.error) {
-      toast({
-        variant: 'destructive',
-        title: 'Uh oh! Something went wrong.',
-        description: (res as any).code,
-      });
-      form.setError('password', { type: 'manual', message: (res as any).code });
-    } else {
-      window.location.reload()
-
-      // Option 2: Programmatic Navigation
-      router.refresh()
-
-      // Option 3: Specific Page Invalidation
-      router.push('/dashboard')
-      //因为GraphQL部分数据也提取的：导致session也同样无法更新了，原来可以的。
-      revalidatePath('/')
-      revalidatePath('/user')
-      router.refresh();
-      // router.push('/');
-      // window.location.href = '/';
-      window.location.reload();
-    }
-  }
-  const signInAction = async (
-      _prevState: string | undefined,
-      formData: FormData,
+  const signInAction = async (_prevState: string | undefined, formData: FormData,
   ) => {
-
-    // e.preventDefault();
     const data = formData;  //{ email: '', password: '' }
     console.log("signInAction formData:{}", formData);
     const response = await signIn('credentials', {
@@ -90,58 +53,15 @@ export default function SignInForm() {
       redirect: false
     });
     if (!response?.error) {
-        //因为GraphQL部分数据也提取的：导致session也同样无法更新了，原来可以的。
-        // revalidatePath('/')
-        // revalidatePath('/user')
-        // environment.resetCache();
         window.location.reload()
-          // Option 2: Programmatic Navigation
-        // router.refresh()
-          // Option 3: Specific Page Invalidation
         router.push('/')
-        // router.push('/');
-        window.location.href = '/';   // “/user”
+        window.location.href = '/';
     } else {
       // setError(response.error);
       // resetForm();
       window.location.href = '/';
     }
-
-    // try {
-    //   await signIn('credentials', Object.fromEntries(formData));
-    // } catch (error) {
-    //   if (!`${error}`.includes('NEXT_REDIRECT')) {
-    //     console.log('Unknown sign in error:', {
-    //       errorText: `${error}`,
-    //       error,
-    //     });
-    //     // Rethrow non-redirect errors
-    //     throw error;
-    //   }
-    // }
-    // redirect(formData.get(KEY_CALLBACK_URL) as string || PATH_ADMIN_PHOTOS);
   };
-
-  // async function signIn(tag,form)
-  // {
-  //   // e.preventDefault();
-  //   let encodePass=sha256().update(form?.password).digest('hex');
-  //   try {
-  //     // setError("");
-  //     // setLoading(true);
-  //     await  submitfunc(form?.username, encodePass);
-  //     //实际await不会在这里阻塞等待的！
-  //     //setIsMeUser(false);  加上这个导致点登陆不管后端应答与否，都会被立刻跳转URL='/'
-  //   } catch (err: any) {
-  //     // setLoading(false);
-  //     // @ts-ignore
-  //     // setError(err.message);
-  //     console.log('Unknown sign in error:', {
-  //       errorText: `${error}`,
-  //       error,
-  //     });
-  //   }
-  // }
 
   const [response, action] = useActionState(signInAction, undefined);
 
