1e6b655313e05a13e24ccb677db3da53c7c10f24
heerzhang <herzhang@163.com>
2025-02-15 22:45:44 +0800

    async MainContent() {  await connection();才能BUILD的

--- mainProj/src/app/MainContent.tsx
+++ mainProj/src/app/MainContent.tsx
@@ -1,15 +1,20 @@
 "use client";
 
+import { connection } from 'next/server'
 import { MainContentQuery } from "./__generated__/MainContentQuery.graphql";
 import { Suspense, lazy } from "react";
 import { graphql, useLazyLoadQuery } from "react-relay";
 import { useRouter, usePathname, useSearchParams } from 'next/navigation'
 
+export const dynamic = 'force-dynamic'
+export const fetchCache = 'default-no-store'
+
 const SlowContentLazy = lazy(() => import("@/app/lazy/SlowContent"));
 
-export function MainContent() {
-  const data = useLazyLoadQuery<MainContentQuery>(
-    graphql`
+export async function MainContent() {
+    await connection();
+    const data = useLazyLoadQuery<MainContentQuery>(
+        graphql`
       query MainContentQuery {
         authUser{
                     id,username, person{id,name}
@@ -20,31 +25,30 @@ export function MainContent() {
         ...SlowContent 
       }
     `,
-    {}
-  );
+        {}
+    );
     const router = useRouter();
     console.log("graphql->authUser", data);
     const {authUser} = data;
     //无需登录的URL
-    const isPublic=false;//isPublicAccsess(history.location.pathname);
-    if(!authUser)
-    {
-        if(!isPublic){
+    const isPublic = false;//isPublicAccsess(history.location.pathname);
+    if (!authUser) {
+        if (!isPublic) {
             // router.push('/login');
             // if (typeof window === "undefined") { } else { window.location.href = "/login"; }
             return null;
         }
     }
 
-  return (
-    <>
-      <main className="text-xl text-green-500">Main data: {data.authUser?.username}</main>
+    return (
+        <>
+            <main className="text-xl text-green-500">Main data: {data.authUser?.username}</main>
 
-      <Suspense fallback={<div className="text-yellow-500">Loading slow data...</div>}>
-        <SlowContentLazy queryRef={data} />
-      </Suspense>
-    </>
-  );
+            <Suspense fallback={<div className="text-yellow-500">Loading slow data...</div>}>
+                <SlowContentLazy queryRef={data}/>
+            </Suspense>
+        </>
+    );
 }
 //排除 不登录的人也允许访问
 /**
