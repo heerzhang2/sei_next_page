e2faf5f4bdcefb6e9ce176c0f669638e390a0c9c
herzhang <herzhang@163.com>
2025-03-07 12:02:50 +0800

    编辑器如何也上TableOfContents

--- mainProj/src/app/rep/SLIDING_JJ/1/[repId]/page.tsx
+++ mainProj/src/app/rep/SLIDING_JJ/1/[repId]/page.tsx
@@ -47,33 +47,34 @@ export default function Page() {
               </div>
           </div>
           <>
-              <Button className="block xl:hidden fixed bottom-0 right-0" variant="outline" onClick={() => setIsDialogOpen(true)}>
-                  Outline
-              </Button>
               <div className="hidden xl:block w-64 shrink-0 border-l p-4">
                   <div className="sticky top-16">
                       <TableOfContents items={tableOfContentsItems} />
                   </div>
               </div>
-              <Drawer.Root open={isDialogOpen} onOpenChange={setIsDialogOpen} direction={"right"}>
-                  <Drawer.Trigger className="relative flex h-10 flex-shrink-0 items-center justify-center gap-2 overflow-hidden rounded-full bg-white px-4 text-sm font-medium shadow-sm transition-all hover:bg-[#FAFAFA] dark:bg-[#161615] dark:hover:bg-[#1A1A19] dark:text-white">
-                      Open Drawer
-                  </Drawer.Trigger>
-                  <Drawer.Portal>
-                      <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-                      <Drawer.Content
-                          className={`bg-white flex flex-col fixed top-0 right-0 
-                            rounded-l-[10px] w-[50vw] min-w-[15rem] max-w-[30rem] h-[100vh]
-                          `}
-                      >
-                          <div className="w-full h-full overflow-auto p-4 rounded-t-[10px]">
-                              <Drawer.Title className="font-medium text-gray-900 mt-8">New Project
-                              </Drawer.Title>
-                              <TableOfContents items={tableOfContentsItems} />
-                          </div>
-                      </Drawer.Content>
-                  </Drawer.Portal>
-              </Drawer.Root>
+              <div style={{ position: 'fixed', inset: 0, pointerEvents: 'none' }}>
+                  <div style={{ pointerEvents: 'auto' }}>
+                      <Drawer.Root open={isDialogOpen} onOpenChange={setIsDialogOpen} direction={"right"}>
+                          <Drawer.Trigger className="flex xl:hidden fixed bottom-0 right-0 h-10 flex-shrink-0 items-center justify-center gap-2 overflow-hidden rounded-full bg-white px-4 text-sm font-medium shadow-sm transition-all hover:bg-[#FAFAFA] dark:bg-[#161615] dark:hover:bg-[#1A1A19] dark:text-white">
+                            Outline
+                          </Drawer.Trigger>
+                          <Drawer.Portal>
+                              <Drawer.Overlay className="fixed inset-0 bg-black/40" />
+                              <Drawer.Content
+                                  className={`bg-white flex flex-col fixed top-0 right-0 
+                                        rounded-l-[10px] w-[50vw] min-w-[15rem] max-w-[30rem] h-[100vh]
+                                      `}>
+                                  <div className="w-full h-full overflow-auto p-4 rounded-t-[10px]">
+                                      <Drawer.Title className="font-medium text-gray-900 mt-8"></Drawer.Title>
+                                      <TableOfContents items={tableOfContentsItems} />
+                                      <Drawer.Description className="leading-6 mt-2 text-gray-600"></Drawer.Description>
+                                  </div>
+                              </Drawer.Content>
+                          </Drawer.Portal>
+                      </Drawer.Root>
+                  </div>
+              </div>
+
           </>
       </>
   );
