994bf3be6c7a241fbbf79560f35fc37e5060e338
heerzhang <herzhang@163.com>
2025-04-26 06:41:45 +0800

    graphcache-v3确认可以在后端宕机情形，依旧能修改报告和submit表单，发送net失败后，后端重启动，验证数据库已经修改成宕机期间变更后报告数据

--- mainProj/src/app/(auth)/user/MainContent.tsx
+++ mainProj/src/app/(auth)/user/MainContent.tsx
@@ -13,15 +13,15 @@ export async function MainContent() {
     const session = await auth();
 
     if (!session?.user) {
-        return <div>未登录啊!session?.user!session?.user</div>;
+        return <div>还未成功登录!</div>;
     }
 //【问题】session?.user是存在的，也不代表用户登录到了真正后端服务器，仅代表前端服务器有连接信息的。
   return (
       <>
-          <main className="text-xl text-green-500">M需要鉴别身份的路径钱全2data:
-              为啥【{session?.user.id}】有吗
-              <div>用户名{session.user.name ?? session.user.email}</div>
-              <Link href="/"> Home __</Link>
+          <main className="text-xl text-green-500">登录后获取当前用户信息:
+              用户id={session?.user.id}
+              <div>用户名是： {session.user.name ?? session.user.email}</div>
+              <Link href="/">回首页</Link>
           </main>
       </>
   );
