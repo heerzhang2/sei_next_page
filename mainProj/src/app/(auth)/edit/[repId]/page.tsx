66c02b2df118fe1f1da1a17f12fee431072b1a99
herzhang <herzhang@163.com>
2025-04-29 08:02:41 +0800

    断网宕机恢复自动提交：直接刷新页面graphql是会主动发出"原始记录已在其它设备或其他人改动了，版本是271旧版本是270"

--- mainProj/src/app/(auth)/edit/[repId]/page.tsx
+++ mainProj/src/app/(auth)/edit/[repId]/page.tsx
@@ -152,3 +152,7 @@ export default function Page({
         </article>
     )
 }
+/*测试
+http://192.168.171.3:3765/edit/ 页面：断网宕机恢复自动提交，前提是form提交后updateTodoResult.fetching一直是Submitting，后面链接点击切换路由，刷新，短暂出现旧版本的取值，随后才是新的更新值
+只能submit一次？；页面直接刷新graphql是会主动发出"原始记录已在其它设备或其他人改动了，版本是271旧版本是270"
+* */
