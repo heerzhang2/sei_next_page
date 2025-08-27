8c0493be1ea0633b0e725a89e45ff6e7f04039e8
herzhang <herzhang@163.com>
2025-08-27 11:49:18 +0800

    循环- 居然和Layout.tsx里面有关系？-2

--- packages/mainProj/tailwind.config.ts
+++ packages/mainProj/tailwind.config.ts
@@ -3,12 +3,13 @@ import type { Config } from "tailwindcss";
 // type DarkModeStrategy = false | 'media' | 'class' | ['class', string] | 'selector' | ['selector', string] | ['variant', string | string[]];
 
 const config: Config = {
-    darkMode: ["class",""],
+    darkMode: "class",
     content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "*.{js,ts,jsx,tsx,mdx}"
+    "*.{js,ts,jsx,tsx,mdx}",
+	"./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
   	extend: {
