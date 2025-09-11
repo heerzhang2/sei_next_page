4c32b8cb71074b82224986529434b829178a0c8b
herzhang <herzhang@163.com>
2025-09-11 13:47:53 +0800

    next.config.ts简化出问题：离线graphql保存丢失

--- test/next.config.ts
+++ test/next.config.ts
@@ -32,6 +32,17 @@ module.exports = async (phase) => {
             webpackBuildWorker: true,
         },
 
+        webpack: (config, { isServer }) => {
+            // 排除所有 .node 二进制文件
+            config.module.rules.push({
+                test: /\.node$/,
+                use: "ignore-loader",
+            })
+            // 如果你需要使用这些模块，可以使用 node-loader 代替 ignore-loader
+            // 但这通常只在 Node.js 环境中有效，不适用于浏览器
+            return config
+        },
+
         output: process.env.BUILD_STANDALONE === "true" ? "standalone" : undefined,
 
         headers: async () => {
