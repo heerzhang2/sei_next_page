c830095ef3deba2ed25c904df6d3ccb4f5427d72
heerzhang <herzhang@163.com>
2025-10-01 12:15:57 +0800

    点击浏览器链接进入本应用或者重新加载页面之后，后端恢复在线会发出多条离线变更

--- packages/mainProj/src/auth/graphql-component.tsx
+++ packages/mainProj/src/auth/graphql-component.tsx
@@ -654,6 +654,7 @@ export function GraphQLProvider({ children }: { children: ReactNode }) {
         const client = createClient({
             url: `${epoint}/graphql`,
             exchanges: [
+                cache,
                 errorExchange({
                     onError: (error, operation) => {
                         if (isVersionConflictError(error)) {
@@ -708,7 +709,6 @@ export function GraphQLProvider({ children }: { children: ReactNode }) {
                         }
                     },
                 }),
-                cache,
                 makeAuthExchange(accessToken, update, print),
                 updateBackendStatusExchange(updateGraphQLBackendStatus, storage),
                 ssr,
