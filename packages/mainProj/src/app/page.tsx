0c80aa0ee8cd65554c7d69e0f57cf7131f64c3d7
heerzhang <herzhang@163.com>
2025-10-06 11:01:43 +0800

    小概率丢失更新json其中1/2个的，考虑大池子列表消除已经完成的和补充更新

--- packages/mainProj/src/app/page.tsx
+++ packages/mainProj/src/app/page.tsx
@@ -44,9 +44,11 @@ export default function Home() {
                             <Link href="/rep/yAEq8hveSa-ZXgUyKHEEHVJlcG9ydA/INDPL_DJ/1">查阅当前1份报告-管道的</Link>
                         </div>
                         <div className="mt-10">
+                            <Link href="/login">登录。。。</Link>
+                        </div>
+                        <div className="mt-10">
                             <Link href="/rep/dP1At1q3QjidlXzd0wSsSlJlcG9ydA/SLIDING_JJ/1">查阅当前2份报告-滑行车</Link>
                         </div>
-
                         <div className="mt-10">
                             <Link href="/rep/yAEq8hveSa-ZXgUyKHEEHVJlcG9ydA/INDPL_DJ/1/_Controller?modelkey=THICK_MS#ProjectList">
                                 查阅当前第1子报告
@@ -68,9 +70,6 @@ export default function Home() {
                         </div>
                         <div className="mt-10">
                             <Link href="/process-start">⬅️ 验证rr登录</Link>
-                        </div>
-                        <div className="mt-10">
-                            <Link href="/login">登录</Link>
                         </div>
                     </li>
                     <li>Save and see your changes instantly.</li>
